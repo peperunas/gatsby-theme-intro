c2cbae2ec559ede1774956b25593f3a574b272e3
Giulio De Pasquale <git@depasquale.giugl.io>
2025-04-18 18:24:49 +0100

    tools, skills killed

--- gatsby-theme-intro/src/types/profile.js
+++ gatsby-theme-intro/src/types/profile.js
@@ -3,15 +3,15 @@ import { arrayOf, bool, number, shape, string, object } from "prop-types"
 
 export const ProfileType = {
   about: string.isRequired,
-  budget: shape({
-    currency: string.isRequired,
-    default: number.isRequired,
-    max: number.isRequired,
-    min: number.isRequired,
-  }).isRequired,
+  // budget: shape({
+  //   currency: string.isRequired,
+  //   default: number.isRequired,
+  //   max: number.isRequired,
+  //   min: number.isRequired,
+  // }).isRequired,
   company: string.isRequired,
-  focus: string.isRequired,
-  focus_url: string,
+  // focus: string.isRequired,
+  // focus_url: string,
   for_hire: bool.isRequired,
   image: shape({
     childImageSharp: object.isRequired,
@@ -22,22 +22,22 @@ export const ProfileType = {
   name: string.isRequired,
   profession: string.isRequired,
   relocation: bool.isRequired,
-  skills: arrayOf(string).isRequired,
-  tools: arrayOf(string).isRequired,
+  // skills: arrayOf(string).isRequired,
+  // tools: arrayOf(string).isRequired,
 }
 
 export const query = graphql`
   fragment ProfileFragment on ProfileYaml {
     about
-    budget {
-      currency
-      default
-      max
-      min
-    }
+   # budget {
+   #   currency
+   #   default
+   #   max
+   #   min
+   # }
     company
-    focus
-    focus_url
+   # focus
+   # focus_url
     for_hire
     image {
       childImageSharp {
@@ -50,7 +50,7 @@ export const query = graphql`
     name
     profession
     relocation
-    skills
-    tools
+  # skills
+  # tools
   }
 `
